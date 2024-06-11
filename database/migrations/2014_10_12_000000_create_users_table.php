defc2ab526c469f6ea68dcc9f48d652da80b827c
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-06-11 14:46:35 +0700

    "Update UserFactory and users table migration, remove roles table migration"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,16 +14,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id(); // Sama dengan BIGINT, auto-increment
             $table->timestamps(); // Otomatis membuat created_at dan updated_at
-            $table->string('username')->unique();
+            $table->string('username')->nullable()->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->string('fullname');
-            $table->string('phone', 24);
+            $table->char('phone', 24)->nullable();
             $table->string('avatar')->nullable(); // Menambahkan nullable
-            $table->string('dbirth')->nullable(); // Menambahkan nullable
-            $table->binary('nic_photo'); // Tipe data untuk menyimpan file gambar
+            $table->string('pbirth')->nullable(); // Menambahkan nullable
+            $table->date('dbirth')->nullable(); // Menambahkan nullable
+            $table->char('nic', 16)->nullable(); // Tipe data untuk menyimpan file gambar
+            $table->string('nic_photo')->nullable(); // Tipe data untuk menyimpan file gambar
         });
     }
 
