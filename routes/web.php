8a85dc069a335c788807f5031eaa1da531700a9f
cintaadenia <cintaadenia238@gmail.com>
2024-06-11 21:47:04 +0700

    layouts user

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/seller', function () {
     return view('penjualan.penjualan');
 });
 
-Route::get('/home', function () {
+Route::get('/user', function () {
     return view('user.home');
 });
 
