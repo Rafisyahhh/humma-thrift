c51d2947c222215006a8eba62525fa327972bd26
nasyasrv <163367751+nasyasrv@users.noreply.github.com>
2024-06-13 09:37:04 +0700

    admin

--- resources/views/layouts/partials/app/navbar.blade.php
+++ resources/views/layouts/partials/app/navbar.blade.php
@@ -23,7 +23,7 @@
             @include('layouts.partials.app.dark-mode')
 
             <!-- Language -->
-            <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
+            {{-- <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <i class="ti ti-language rounded-circle ti-md"></i>
                 </a>
@@ -59,11 +59,11 @@
                         </a>
                     </li>
                 </ul>
-            </li>
+            </li> --}}
             <!--/ Language -->
 
             <!-- Quick links  -->
-            <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
+            {{-- <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown"
                     data-bs-auto-close="outside" aria-expanded="false">
                     <i class="ti ti-layout-grid-add ti-md"></i>
@@ -146,7 +146,7 @@
                         </div>
                     </div>
                 </div>
-            </li>
+            </li> --}}
             <!-- Quick links -->
 
             <!-- Notification -->
