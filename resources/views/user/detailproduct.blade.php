d62ced930d0e1a156e596a21b90f6d42da4b563f
nugiartika <141117388+nugiartika@users.noreply.github.com>
2024-06-19 13:17:32 +0700

    detail produk

--- resources/views/user/detailproduct.blade.php
+++ resources/views/user/detailproduct.blade.php
@@ -79,7 +79,7 @@
                             <div class="row">
                                 <div class="product-quantity" style="display: flex; align-items: center; gap: 10px;">
                                     <div class="quantity-wrapper">
-                                        <div class="wishlist">
+                                        <div class="wishlist" style="border:none;">
                                             <span>
                                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                                     xmlns="http://www.w3.org/2000/svg">
@@ -91,23 +91,12 @@
                                             </span>
                                         </div>
                                     </div>
-                                    {{-- <div class="share-icons">
-                                    <a href="/user/checkout" class="share-icon">
-                                        <span>
-                                            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <rect width="40" height="40" rx="20" fill="white" />
-                                                <g transform="translate(7.7, 7.7)">
-                                                    <path fill="currentColor" d="M9 8V6h6v2zM7 22q-.825 0-1.412-.587T5 20t.588-1.412T7 18t1.413.588T9 20t-.587 1.413T7 22m10 0q-.825 0-1.412-.587T15 20t.588-1.412T17 18t1.413.588T19 20t-.587 1.413T17 22M1 4V2h3.275l4.25 9h7l3.9-7H21.7l-4.975 9H8.1L7 15h12v2H3.625L6.6 11.6L3 4z" />
-                                                </g>
-                                            </svg>
-                                        </span>
-                                    </a>
-                                    </div> --}}
+
                                     <div class="share-icons">
                                         <a href="#" class="share-icon">
                                             <span>
                                                 <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <rect width="40" height="40" rx="20" fill="white" />
+                                                    <rect width="40" height="40" rx="20" />
                                                     <g transform="translate(7.7, 7.7)">
                                                         <path fill="currentColor" d="M9 8V6h6v2zM7 22q-.825 0-1.412-.587T5 20t.588-1.412T7 18t1.413.588T9 20t-.587 1.413T7 22m10 0q-.825 0-1.412-.587T15 20t.588-1.412T17 18t1.413.588T19 20t-.587 1.413T17 22M1 4V2h3.275l4.25 9h7l3.9-7H21.7l-4.975 9H8.1L7 15h12v2H3.625L6.6 11.6L3 4z" />
                                                     </g>
@@ -174,7 +163,7 @@
 
 
     <section class="product product-description">
-        <div class="container">m
+        <div class="container">
             <div class="product-detail-section">
                 <nav>
                     <div class="nav nav-tabs nav-item" id="nav-tab" role="tablist">
