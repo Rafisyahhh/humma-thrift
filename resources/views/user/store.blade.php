d65e7170805017948d15c46cafc5975699c030d6
nasyasrv <163367751+nasyasrv@users.noreply.github.com>
2024-06-25 13:49:44 +0700

    chart seller

--- resources/views/user/store.blade.php
+++ resources/views/user/store.blade.php
@@ -24,7 +24,7 @@
                                 </div>
                             </div>
                             <div class="product-cart-btn">
-                                <a href="{{ route('store.profile', 'cinta66') }}" class="product-btn">Detail</a>
+                                <a href="{{ route('store.profile', 'nasyashop71') }}" class="product-btn">Detail</a>
                             </div>
                         </div>
                     </div>
