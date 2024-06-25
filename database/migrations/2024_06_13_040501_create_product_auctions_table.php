1f2c37483d85e6e13c62ed489708c2b1b9511572
SyauqiAli <sauqi2019@gmail.com>
2024-06-25 10:58:13 +0700

    fix

--- database/migrations/2024_06_13_040501_create_product_auctions_table.php
+++ database/migrations/2024_06_13_040501_create_product_auctions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
         Schema::create('product_auctions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->foreignId('store_id')->constrained();
+            $table->foreignId('store_id')->constrained("user_stores");
             $table->foreignId('brand_id')->constrained();
             $table->string('title')->unique();
             $table->text('description');
