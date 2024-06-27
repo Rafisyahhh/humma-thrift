51eec884e0312a2a8503711dd8938fea31d4ca8d
cintaadenia <cintaadenia238@gmail.com>
2024-06-27 11:10:03 +0700

    wishlist

--- resources/views/user/wishlist.blade.php
+++ resources/views/user/wishlist.blade.php
@@ -1,70 +1,238 @@
-@extends('layouts.panel')
-@section('css')
-<style>
-    .table-row.ticket-row:hover {
-        background-color: rgba(28, 56, 121, 0.1)!important;
-    }
-</style>
-@endsection
+@extends('layouts.home')
+
+@section('title', 'Wishlist')
+
+@push('style')
+    <style>
+        a.wishlist-link {
+            padding: 1rem 1.75rem;
+            margin-right: auto;
+        }
+
+        .product-wrapper .product-cart-btn {
+            left: 0;
+            position: absolute;
+            bottom: 1rem;
+            right: 0;
+            transition: all 0.4s;
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            bottom: 0;
+        }
+
+    </style>
+@endpush
+
 @section('content')
-<div class="wishlist">
-    <div class="cart-content">
-        <h5 class="cart-heading mb-3">Daftar Favorit</h5>
-    </div>
-    <div class="cart-section wishlist-section">
-        <table>
-            <tbody>
-                <tr class="table-row table-top-row">
-                    <td class="table-wrapper wrapper-product">
-                        <h5 class="table-heading">PRODUK</h5>
-                    </td>
-                    <td class="table-wrapper">
-                        <div class="table-wrapper-center">
-                            <h5 class="table-heading">STOK</h5>
-                        </div>
-                    </td>
-                    <td class="table-wrapper">
-                        <div class="table-wrapper-center">
-                            <h5 class="table-heading">AKSI</h5>
-                        </div>
-                    </td>
-                </tr>
-                <tr class="table-row ticket-row">
-                    <td class="table-wrapper wrapper-product">
-                        <div class="wrapper">
-                            <div class="wrapper-img">
-                                <img src="{{asset ('template-assets/front/assets/images/homepage-one/product-img/product-img-1.webp') }}"
-                                    alt="img">
+    <section class="product product-sidebar footer-padding">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-3">
+                    <div class="sidebar" data-aos="fade-right">
+                        <h4 class="wrapper-heading">Semua Wishlist</h4> <br><br>
+                        <div class="sidebar-section">
+                            <div class="sidebar-wrapper">
+                                <h5 class="wrapper-heading">Kategori Produk</h5>
+                                <div class="sidebar-item">
+                                    <ul class="sidebar-list">
+                                        {{-- @foreach ($categories as $item) --}}
+                                            <li>
+                                                <input type="checkbox" id="bags" name="bags">
+                                                <label for="bags">hhhhh</label>
+                                            </li>
+                                        {{-- @endforeach --}}
+                                    </ul>
+                                </div>
                             </div>
-                            <div class="wrapper-content">
-                                <h5 class="heading">Classic Design Skart</h5>
+                            <hr>
+                            <div class="sidebar-wrapper">
+                                <h5 class="wrapper-heading">Brands</h5>
+                                <div class="sidebar-item">
+                                    <ul class="sidebar-list">
+                                        {{-- @foreach ($brands as $item) --}}
+                                            <li>
+                                                <input type="checkbox" id="bags" name="bags">
+                                                <label for="bags">Brand</label>
+                                            </li>
+                                        {{-- @endforeach --}}
+                                    </ul>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="sidebar-wrapper">
+                                <h5 class="wrapper-heading">Warna</h5>
+                                <div class="sidebar-item">
+                                    <ul class="sidebar-list">
+                                        <li>
+                                            <input type="checkbox" id="red" name="red">
+                                            <label for="red">Merah</label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="blue" name="blue">
+                                            <label for="blue">Biru</label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="navy" name="navy">
+                                            <label for="navy">Navy</label>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="sidebar-wrapper">
+                                <h5 class="wrapper-heading">Ukuran</h5>
+                                <div class="sidebar-item">
+                                    <ul class="sidebar-list">
+                                        <li>
+                                            <input type="checkbox" id="small" name="small">
+                                            <label for="small">Kecil</label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="medium" name="medium">
+                                            <label for="medium">Sedang</label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="large" name="large">
+                                            <label for="large">Besar</label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="xl" name="xl">
+                                            <label for="xl">XL</label>
+                                        </li>
+                                        <li>
+                                            <input type="checkbox" id="2xl" name="2xl">
+                                            <label for="2xl">2XL</label>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </td>
-                    <td class="table-wrapper">
-                        <div class="table-wrapper-center">
-                            <h5 class="heading">$20.00</h5>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="product-sidebar-section" data-aos="fade-up">
+                        <div class="row g-5">
+                            <div class="col-lg-12">
+                                <div class="product-sorting-section">
+                                    <div class="result ms-auto me-4">
+                                        <h6 style="font-size: 1.5rem;">Urutkan</h6>
+                                    </div>
+                                    <div class="btn-group mt-2">
+                                        <div class="dropdown">
+                                            <a class="css-71s6qs d-flex align-items-center gap-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                <span>Terbaru Disimpan</span>
+                                                <i class="fas fa-chevron-down"></i>
+                                            </a>
+                                            <div class="btn-group">
+                                                <div class="dropdown">
+                                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="width: 20rem; text-align: center; font-size: 16px;">
+                                                        <li><a class="dropdown-item" href="#" style="font-size: 16px;">Terbaru Disimpan</a></li><hr>
+                                                        <li><a class="dropdown-item" href="#" style="font-size: 16px;">Terlama Disimpan</a></li><hr>
+                                                        <li><a class="dropdown-item" href="#" style="font-size: 16px;">Ulasan Terbanyak</a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- @forelse ($product as $item) --}}
+
+                                <div class="result mb-6">
+                                    <p><strong style="font-size: 1.5rem">1</strong> <span>Barang</span></p>
+                                </div>
+
+                                <div class="col-lg-4 col-sm-6">
+
+                                    <div class="product-wrapper" data-aos="fade-up">
+                                        <div class="product-img">
+                                            <img src="{{ asset('template-assets/front/assets/images/homepage-one/product-img/product-img-5.webp') }}" alt="product-img"
+                                                class="object-fit-cover">
+                                        </div>
+                                        <div class="product-info">
+                                            <div class="product-description">
+                                                <a href="" class="product-details">title
+                                                </a>
+                                                <div class="price">
+                                                    <span
+                                                        class="new-price">Rp.9999</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="product-cart-btn">
+                                            <div class="dropdown">
+                                                <a class="wishlist-link" href="#" role="button" id="wishlistDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-h"></i>
+                                                </a>
+                                                <ul class="dropdown-menu" aria-labelledby="wishlistDropdown">
+                                                    <li><a class="dropdown-item" href="#">Hapus Favorit</a></li>
+                                                    <hr>
+                                                    <li><a class="dropdown-item" href="#">Batal</a></li>
+                                                </ul>
+                                            </div>
+
+                                            <a href="/user/checkout" class="product-btn">
+                                                <i class="fas fa-shopping-cart"></i>
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            {{-- @empty
+                            <div class="col-lg-12">
+                                <h3 class="text-center">Produk Masih Kosong</h3>
+                                <p class="text-center">Maaf ya, kami masih belum menambahkan produknya. Tapi dalam waktu dekat kami akan menambahkan beberapa produk untukmu, stay tune.</p>
+                            </div>
+                            @endforelse --}}
+
+                            {{-- @forelse ($product_auction as $item) --}}
+                                <div class="col-lg-4 col-sm-6">
+                                    <div class="product-wrapper" data-aos="fade-up">
+                                        <div class="product-img">
+                                            <img src="{{ asset('template-assets/front/assets/images/homepage-one/product-img/product-img-5.webp') }}" alt="product-img"
+                                                class="object-fit-cover">
+                                        </div>
+                                        <div class="product-info">
+                                            <div class="product-description">
+                                                <a href="" class="product-details">uuuuuu
+                                                </a>
+                                                <div class="price">
+                                                    <span
+                                                        class="new-price">Rp.0000
+                                                        - Rp.9999</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="product-cart-btn">
+                                            <div class="dropdown">
+                                                <a class="wishlist-link" href="#" role="button" id="wishlistDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-h"></i>
+                                                </a>
+                                                <ul class="dropdown-menu" aria-labelledby="wishlistDropdown">
+                                                    <li><a class="dropdown-item" href="#">Hapus Favorit</a></li>
+                                                    <hr>
+                                                    <li><a class="dropdown-item" href="#">Batal</a></li>
+                                                </ul>
+                                            </div>
+
+                                            <a href="/user/checkout" class="product-btn">
+                                                <i class="fas fa-shopping-cart"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            {{-- @empty
+                            <div class="col-lg-12">
+                                <h3 class="text-center">Produk Lelang Masih Kosong</h3>
+                                <p class="text-center">Maaf ya, kami masih belum menambahkan produknya. Tapi dalam waktu dekat kami akan menambahkan beberapa produk untukmu, stay tune.</p>
+                            </div>
+                            @endforelse --}}
                         </div>
-                    </td>
-                    <td class="table-wrapper">
-                        <div class="table-wrapper-center">
-                            <span>
-                                <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M9.7 0.3C9.3 -0.1 8.7 -0.1 8.3 0.3L5 3.6L1.7 0.3C1.3 -0.1 0.7 -0.1 0.3 0.3C-0.1 0.7 -0.1 1.3 0.3 1.7L3.6 5L0.3 8.3C-0.1 8.7 -0.1 9.3 0.3 9.7C0.7 10.1 1.3 10.1 1.7 9.7L5 6.4L8.3 9.7C8.7 10.1 9.3 10.1 9.7 9.7C10.1 9.3 10.1 8.7 9.7 8.3L6.4 5L9.7 1.7C10.1 1.3 10.1 0.7 9.7 0.3Z"
-                                        fill="#AAAAAA"></path>
-                                </svg>
-                            </span>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    {{-- <div class="wishlist-btn">
-        <a href="#" class="clean-btn">Clean Wishlist</a>
-        <a href="#" class="shop-btn">View Cards</a>
-    </div> --}}
-</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
