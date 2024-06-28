e83cf0d6321ac6463ef7e14597bdcd13a55dc89c
SyauqiAli <sauqi2019@gmail.com>
2024-06-28 16:47:09 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,16 +5,18 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
+            UserStoreSeeder::class,
+            ProductCategorySeeder::class,
+            BrandSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
