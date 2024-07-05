cc6c7808c2f377e94624c0d6f70bbe17768e8498
SyauqiAli <sauqi2019@gmail.com>
2024-07-05 10:42:33 +0700

    R

--- resources/views/admin/produk.blade.php
+++ resources/views/admin/produk.blade.php
@@ -189,7 +189,7 @@
       </div>
     </div>
   @endforeach
-  {{-- fire --}}
+  {{-- fir --}}
   <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
