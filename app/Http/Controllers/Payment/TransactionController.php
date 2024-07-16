34a7abdf7e352f2b210f7788005e0f35662b8424
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-07-16 14:32:56 +0700

    "Refactor TransactionController: added try-catch blocks, notification, and error handling"

--- app/Http/Controllers/Payment/TransactionController.php
+++ app/Http/Controllers/Payment/TransactionController.php
@@ -9,7 +9,9 @@ use App\Models\Favorite;
 use App\Models\Order;
 use App\Models\TransactionOrder;
 use App\Models\UserAddress;
+use App\Notifications\NotificationUserCheckout;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
@@ -18,37 +20,47 @@ class TransactionController extends Controller
      */
     public function store(Request $request)
     {
-        $products = session('checkouted_product');
-        $method = $request->method;
-        $address = UserAddress::find($request->addressOption);
-        $product = Product::whereIn('id', $products)->get();
-        $tripay = new TripayController();
-        $transaction = $tripay->requestTransaction($method, $product);
+        try {
+            $products = session('checkouted_product');
+            $method = $request->method;
+            $address = UserAddress::find($request->addressOption);
+            $product = Product::whereIn('id', $products)->get();
+            $tripay = new TripayController();
+            $transaction = $tripay->requestTransaction($method, $product);
 
-        if (isset($transaction->error)) {
-            // Handle the error appropriately, e.g., return an error response
-            return back()->withErrors(['error' => $transaction->error]);
-        }
+            if (isset($transaction->error)) {
+                return back()->withErrors(['error' => $transaction->error]);
+            }
 
-        $order = TransactionOrder::create([
-            'user_id' => auth()->id(),
-            // 'product_id' => $request->product_id,
-            'user_address_id' => $address->id,
-            'transaction_id' => $transaction['merchant_ref'],
-            'reference_id' => $transaction['reference'],
-            'total' => $transaction['amount'],
-            'delivery_status' => 'selesaikan pesanan',
-            'status' => $transaction['status'],
-        ]);
-
-        $product->map(function($item) use ($order) {
-            Order::create([
-                'product_id' => $item->id,
-                'transaction_order_id' => $order->id
+            # Buat Data Transaksi
+            $transactions = TransactionOrder::create([
+                'user_id' => auth()->id(),
+                'user_address_id' => $address->id,
+                'transaction_id' => $transaction['merchant_ref'],
+                'reference_id' => $transaction['reference'],
+                'total' => $transaction['amount'],
+                'delivery_status' => 'selesaikan pesanan',
+                'status' => $transaction['status'],
             ]);
-        });
 
-        return redirect()->route('user.transaction.show', ['reference' => $transaction['reference']]);
+            # Buat Data Daftar Order
+            $product->map(function ($item) use ($transactions) {
+                Order::create([
+                    'product_id' => $item->id,
+                    'transaction_order_id' => $transactions->id
+                ]);
+            });
+
+            # Kirim Notifikasi
+            Auth::user()->notify(new NotificationUserCheckout($transactions));
+
+            # Redirect ke halaman transaksi
+            return redirect()->route('user.transaction.show', ['reference' => $transaction['reference']]);
+        } catch (\Throwable $th) {
+            return back()->with('error', $th->getMessage());
+        } catch (\Exception $e) {
+            return back()->with('error', $e->getMessage());
+        }
     }
 
 
