64abdfcc51941103dcd2b288d8b0969ca8711b6f
cintaadenia <cintaadenia238@gmail.com>
2024-07-22 14:25:27 +0700

    route landing

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -201,7 +201,7 @@
                                                 {{ $even->subjudul }}
                                             </h5>
                                             <h1 class="wrapper-details" style="color:#1c3879;">{{ $even->judul }}</h1>
-                                            <a href="product-sidebar.html" class="shop-btn mt-3"
+                                            <a href="{{ url('/product/regular') }}" class="shop-btn mt-3"
                                                 style="color: #1c3879;">Belanja Sekarang</a>
                                         </div>
                                     </div>
