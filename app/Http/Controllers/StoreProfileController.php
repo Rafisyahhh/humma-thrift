4ddea239438c72740e1aac25e18559dcc95cbcab
SyauqiAli <sauqi2019@gmail.com>
2024-07-24 13:32:28 +0700

    fix

--- app/Http/Controllers/StoreProfileController.php
+++ app/Http/Controllers/StoreProfileController.php
@@ -13,62 +13,54 @@ use Auth;
 use Illuminate\Http\Request;
 use Jorenvh\Share\ShareFacade as Share;
 
-class StoreProfileController extends Controller
-{
+class StoreProfileController extends Controller {
     /**
      * Showing index of Store Information
      */
-    public function index(UserStore $store)
-    {
+    public function index(UserStore $store) {
         $time = \Carbon\Carbon::now()->format('H:i');
         $isProduct = Product::query()
             ->where('store_id', $store->id)
             ->get();
         $isProductAuction = ProductAuction::where('store_id', $store->id)->get();
-        $countFavorite = Favorite::where('user_id', auth()->id())->count();
+        $reviews = Ulasan::whereHas('product', function ($q) use ($store) {
+            $q->whereIn('slug', explode(',', $request->categories));
+        })->get();
+        dd();
 
-        $carts = cart::where('user_id', auth()->id())
-        ->whereNotNull('product_id')
-        ->orderBy('created_at')
-        ->get();
-        $countcart = cart::where('user_id',auth()->id())->count();
-
-        return view('store.index', compact('store', 'isProduct', 'isProductAuction','carts','countcart', 'countFavorite','time'));
+        return view('store.index', compact('store', 'isProduct', 'isProductAuction', 'time', 'reviews'));
     }
 
     /**
      * Showing list of the products
      */
-    public function products(UserStore $store)
-    {
+    public function products(UserStore $store) {
         dd('products');
     }
 
-    public function productDetail(UserStore $store, string $slug)
-    {
+    public function productDetail(UserStore $store, string $slug) {
         $isProduct = Product::where('slug', $slug)->first();
         $isProductAuction = ProductAuction::where('slug', $slug)->first();
         $carts = cart::where('user_id', auth()->id())
-        ->whereNotNull('product_id')
-        ->orderBy('created_at')
-        ->get();
-        $countcart = cart::where('user_id',auth()->id())->count();
+            ->whereNotNull('product_id')
+            ->orderBy('created_at')
+            ->get();
+        $countcart = cart::where('user_id', auth()->id())->count();
         $countFavorite = Favorite::where('user_id', auth()->id())->count();
         $user = Auth::user();
         $ulasan = Ulasan::all();
-        return view('user.detailproduct', compact('store', 'isProduct', 'isProductAuction','user','carts','countcart', 'countFavorite','ulasan'));
+        return view('user.detailproduct', compact('store', 'isProduct', 'isProductAuction', 'user', 'carts', 'countcart', 'countFavorite', 'ulasan'));
     }
 
-    public function showStore()
-    {
+    public function showStore() {
         $store = UserStore::all();
         $countFavorite = Favorite::where('user_id', auth()->id())->count();
-        $countcart = cart::where('user_id',auth()->id())->count();
+        $countcart = cart::where('user_id', auth()->id())->count();
         $carts = cart::where('user_id', auth()->id())
-        ->whereNotNull('product_id')
-        ->orderBy('created_at')
-        ->get();
+            ->whereNotNull('product_id')
+            ->orderBy('created_at')
+            ->get();
 
-        return view('user.store', compact('store','countFavorite', 'countcart', 'carts'));
+        return view('user.store', compact('store', 'countFavorite', 'countcart', 'carts'));
     }
 }
