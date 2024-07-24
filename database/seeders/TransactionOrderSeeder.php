2158b5026d761fb66e4700beec56025afaf6cd65
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-07-24 10:59:15 +0700

    "Updated database migration and seeder for transaction orders, and modified user location view"

--- database/seeders/TransactionOrderSeeder.php
+++ database/seeders/TransactionOrderSeeder.php
@@ -2,10 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\TransactionOrder;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Str;
 
 class TransactionOrderSeeder extends Seeder
 {
@@ -14,10 +18,37 @@ class TransactionOrderSeeder extends Seeder
      */
     public function run(): void
     {
-        $orders = Product::inRandomOrder()->take(5);
+        $orders = Product::whereNotIn('id', Order::pluck('id'))->inRandomOrder()->limit(4);
+        $user = User::find(18);
+        $dateStart = new Carbon('2024-07-12 12:24:13');
+        $randomRefId = 'INV-' . Str::random(12);
+        $randomTrxId = 'DEV-' . Str::random(12);
 
-        dd($orders->sum('price'));
+        if($orders->get()->isNotEmpty()) {
+            $transaction = TransactionOrder::create([
+                'user_id' => $user->id,
+                'user_address_id' => $user->UserAddress()->first()->id,
+                'total' => $orders->sum('price'),
+                'reference_id' => $randomRefId,
+                'transaction_id' => $randomTrxId,
+                'status' => 'PAID',
+                'expired_at' => $dateStart->addDays(1),
+                'paid_at' => $dateStart->addHours(4),
+                'payment_method' => 'BCA',
+                'total_harga' => $orders->sum('price') + 2500,
+                'biaya_admin' => 2500,
+                'created_at' => $dateStart,
+                'updated_at' => $dateStart->addHours(4),
+            ]);
 
-        // TransactionOrder::create([]);
+            collect($orders->get()->pluck('id'))->each(function($item) use ($user, $dateStart, $transaction) {
+                Order::create([
+                    'product_id' => $item,
+                    'transaction_order_id' => $transaction->id,
+                    'created_at' => $dateStart,
+                    'updated_at' => $dateStart->addHours(4),
+                ]);
+            });
+        }
     }
 }
