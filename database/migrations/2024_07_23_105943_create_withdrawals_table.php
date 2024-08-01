4c4ae30bc3790b7a7f9bc7fe4fc0747b538fcfc2
SyauqiAli <sauqi2019@gmail.com>
2024-08-01 13:58:35 +0700

    seeder

--- database/migrations/2024_07_23_105943_create_withdrawals_table.php
+++ database/migrations/2024_07_23_105943_create_withdrawals_table.php
@@ -6,18 +6,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('withdrawals', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('transaction_id')->nullable();
-            $table->timestamp('finished_at')->useCurrent()->nullable();
+            $table->timestamp('finished_at')->nullable();
             $table->foreignId('bank_id')->constrained();
             $table->string('bank_number');
             $table->foreignId('user_store_id')->constrained();
@@ -31,8 +29,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('withdrawals');
     }
 };
--- database/seeders/WithdrawalSeeder.php
+++ database/seeders/WithdrawalSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\Withdrawal;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Str;
 
-class WithdrawalSeeder extends Seeder
-{
+class WithdrawalSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(): void {
         //
+
+        for ($i = 0; $i < 10; $i++) {
+            Withdrawal::create([
+                'user_id' => 16,
+                'user_store_id' => 1,
+                'bank_id' => random_int(0, 107),
+                'bank_number' => 7974258162899519,
+                'amount' => $this->random_int_with_fixed_suffix(50_000, 5_000_000),
+                'transaction_id' => Str::upper("WTH-{" . Str::random(16) . "}"),
+            ]);
+        }
+    }
+
+    function random_int_with_fixed_suffix($min, $max, $suffix = 0) {
+        $min_adjusted = (int) ($min / 1000);
+        $max_adjusted = (int) ($max / 1000);
+
+        $random_number = random_int($min_adjusted, $max_adjusted);
+
+        return $random_number * 1000 + $suffix;
     }
 }
