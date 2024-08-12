ee866545554e2e79ff9618e17c896458a2ad63dc
cintaadenia <cintaadenia238@gmail.com>
2024-08-12 09:21:35 +0700

    Navbar-bootom .menu-list li:hover::after

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -420,7 +420,7 @@
                                         <div class="product-info">
                                             <div class="product-description">
                                                 <a href="{{ route('store.product.detail', ['store' => $item->userStore->username, 'product' => $item->slug]) }}"
-                                                    class="product-details">
+                                                    class="product-details" style="font-size: 1.78rem">
                                                     {{ $item->title }}
                                                 </a>
                                                 <div class="price">
@@ -505,7 +505,7 @@
                                         <div class="product-info">
                                             <div class="product-description">
                                                 <a href="{{ route('store.product.detail', ['store' => $item->userStore->username, 'product' => $item->slug]) }}"
-                                                    class="product-details">
+                                                    class="product-details" style="font-size: 1.79rem">
                                                     {{ $item->title }}
                                                 </a>
                                                 <div class="price">
