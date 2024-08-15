31500984cd2398d3c0cb0c9975fed63a2907faee
SyauqiAli <sauqi2019@gmail.com>
2024-08-15 01:33:40 +0000

    f

--- resources/views/Landing/components/product-auction.blade.php
+++ resources/views/Landing/components/product-auction.blade.php
@@ -101,52 +101,54 @@
         @endif
       @else
         <div class="product-cart-btn">
-          <a href="{{ url('login') }}" class="product-btn openModal">Ikuti
+          <a href="@role('admin') # @else {{ url('login') }} @endrole" class="product-btn openModal">Ikuti
             Lelang</a>
         </div>
       @endif
 
-      <div id="reviewModal-{{ $item->id }}" class="modal" style="display: none;">
-        <div class="modal-content">
-          <button class="close" style="float: right; text-align: end;"
-            onclick="closeModal2('#reviewModal-{{ $item->id }}')">&times;</button>
-          @if ($user)
-            @if ($existingAuction)
-              <p style="text-align: center; font-size :20px; font-weight:bold; margin-top:2rem;">Anda
-                sudah
-                mengikuti lelang</p>
-              <p style="text-align: center; margin-bottom:2rem;">bid lelang anda :
-                {{ $auctions->auction_price }}</p>
-            @elseif ($auctionproduct)
-              <p style="text-align: center; font-size :20px; font-weight:bold; margin-top: 2rem; margin-bottom:2rem;">
-                lelang sudah berakhir</p>
-            @else
-              <h4 style="text-align: center;">Bid Lelang</h4>
-              <form id="auctionForm-{{ $item->id }}" method="post" action="{{ route('user.auctions.store') }}"
-                class="mt-5">
-                @csrf
-                <input type="hidden" name="product_id" value="{{ $item->id }}">
-                <label for="auction_price" class="form-label" style="font-size: 18px;">Bid Lelang
-                  :</label> <br>
-                <input type="number" name="auction_price"
-                  class="form-control @error('auction_price') is-invalid @enderror"
-                  placeholder="Masukkan Bid Lelang anda" style="font-size: 17px;">
-                <p style="margin-top: 5px;margin-left:6px;font-size:12px;color: #7c7c7c;">
-                  Bid : Rp{{ number_format($item->bid_price_start, null, null, '.') }}
-                  -
-                  Rp{{ number_format($item->bid_price_end, null, null, '.') }}</p>
-                @error('auction_price')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                @enderror
-                <button type="submit" class="shop-btn" style="margin-left: 22rem;">Kirim Bid
-                  Anda</button>
-              </form>
+      @if ($user && $user->hasRole('user'))
+        <div id="reviewModal-{{ $item->id }}" class="modal" style="display: none;">
+          <div class="modal-content">
+            <button class="close" style="float: right; text-align: end;"
+              onclick="closeModal2('#reviewModal-{{ $item->id }}')">&times;</button>
+            @if ($user)
+              @if ($existingAuction)
+                <p style="text-align: center; font-size :20px; font-weight:bold; margin-top:2rem;">Anda
+                  sudah
+                  mengikuti lelang</p>
+                <p style="text-align: center; margin-bottom:2rem;">bid lelang anda :
+                  {{ $auctions->auction_price }}</p>
+              @elseif ($auctionproduct)
+                <p style="text-align: center; font-size :20px; font-weight:bold; margin-top: 2rem; margin-bottom:2rem;">
+                  lelang sudah berakhir</p>
+              @else
+                <h4 style="text-align: center;">Bid Lelang</h4>
+                <form id="auctionForm-{{ $item->id }}" method="post" action="{{ route('user.auctions.store') }}"
+                  class="mt-5">
+                  @csrf
+                  <input type="hidden" name="product_id" value="{{ $item->id }}">
+                  <label for="auction_price" class="form-label" style="font-size: 18px;">Bid Lelang
+                    :</label> <br>
+                  <input type="number" name="auction_price"
+                    class="form-control @error('auction_price') is-invalid @enderror"
+                    placeholder="Masukkan Bid Lelang anda" style="font-size: 17px;">
+                  <p style="margin-top: 5px;margin-left:6px;font-size:12px;color: #7c7c7c;">
+                    Bid : Rp{{ number_format($item->bid_price_start, null, null, '.') }}
+                    -
+                    Rp{{ number_format($item->bid_price_end, null, null, '.') }}</p>
+                  @error('auction_price')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+                  <button type="submit" class="shop-btn" style="margin-left: 22rem;">Kirim Bid
+                    Anda</button>
+                </form>
+              @endif
             @endif
-          @endif
+          </div>
         </div>
-      </div>
+      @endif
       <div id="shareModal-{{ $item->id }}" class="modal" style="display: none;">
         <div class="modal-content">
           <button class="close" style="float: right; text-align: end;"
