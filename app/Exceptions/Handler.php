994e16c311bb97de20612e15d2a6cd324247e155
SyauqiAli <sauqi2019@gmail.com>
2024-08-15 03:44:02 +0000

    "Updated ExceptionHandler to render 403 errors with a view instead of JSON response"

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler {
@@ -45,12 +46,9 @@ class Handler extends ExceptionHandler {
     }
 
     public function render($request, Throwable $exception) {
-        if ($exception instanceof \Illuminate\Auth\Access\AuthorizationException) {
-            if ($request->ajax() || $request->wantsJson()) {
-                return response()->json(['message' => 'Akses ditolak.'], 403);
-            }
+        if ($exception instanceof HttpException && $exception->getStatusCode() == 403) {
+            return response()->view('errors.403', [], 403);
         }
-
         return parent::render($request, $exception);
     }
 
