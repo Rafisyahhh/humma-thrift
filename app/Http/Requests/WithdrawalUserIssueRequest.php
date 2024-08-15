e115f9204d8e4e57e364a6500f3027ba7d7e959e
nugiartika <141117388+nugiartika@users.noreply.github.com>
2024-08-15 09:46:07 +0700

    request penarikan

--- app/Http/Requests/WithdrawalUserIssueRequest.php
+++ app/Http/Requests/WithdrawalUserIssueRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Enums\WithdrawalStatusEnum;
+use App\Models\Order;
 use App\Models\TransactionOrder;
 use App\Models\Withdrawal;
 use Auth;
@@ -28,19 +29,43 @@ class WithdrawalUserIssueRequest extends FormRequest
         return [
             'bank_id' => 'required|exists:banks,id',
             'bank_number' => 'required|string',
-            'amount' => ['required', 'numeric', 'min:1', function($attribute, $value, $fail) {
+            'amount' => ['required', 'numeric', 'min:1', function ($attribute, $value, $fail) {
                 $user = Auth::user();
                 $storeId = $user->getAttribute('store')->id;
                 $netIncome = (int) TransactionOrder::whereHas('order.product.userstore', fn(mixed $item) => $item->where('store_id', $storeId))
                     ->where('status', 'PAID')
-                    ->where('delivery_status', 'selesai')
-                    ->sum('total') * 0.9;
+                    // ->where('delivery_status', 'selesai')
+                    ->sum('total_harga');
+
+                $orderR = Order::with('product.userstore')
+                    ->orderBy('transaction_order_id')
+                    ->get()
+                    ->groupBy('transaction_order_id');
+
+                $orderL = Order::with('product_auction')
+                    ->orderBy('transaction_order_id')
+                    ->get()
+                    ->groupBy('transaction_order_id');
+
+                $hargaBeli = $orderR->sum(function ($orders) {
+                    return $orders->sum(function ($order) {
+                        return $order->product->start_price ?? 0;
+                    });
+                });
+
+                $hargaBeliL = $orderL->sum(function ($orders) {
+                    return $orders->sum(function ($order) {
+                        return $order->product_auction->start_price ?? 0;
+                    });
+                });
+
+                $saldo = ($hargaBeli + $hargaBeliL) + (($netIncome - ($hargaBeli + $hargaBeliL)) * 0.9);
 
                 $withdrawalTotal = (int) Withdrawal::where('user_id', $user->id)
                     ->where('status', WithdrawalStatusEnum::COMPLETED)
                     ->sum('amount');
 
-                $accountBalance = $netIncome - $withdrawalTotal;
+                $accountBalance = $saldo - $withdrawalTotal;
 
                 if ((int) $value > $accountBalance) {
                     $fail('Jumlah penarikan melebihi saldo akun yang tersedia.');
