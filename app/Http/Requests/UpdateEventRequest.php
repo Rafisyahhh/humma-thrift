b0393f710da52b7aebc267c00f858590eea93afd
SyauqiAli <sauqi2019@gmail.com>
2024-08-19 13:16:52 +0700

    f

--- app/Http/Requests/UpdateEventRequest.php
+++ app/Http/Requests/UpdateEventRequest.php
@@ -4,13 +4,11 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateEventRequest extends FormRequest
-{
+class UpdateEventRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
+    public function authorize(): bool {
         return true;
     }
 
@@ -19,24 +17,22 @@ class UpdateEventRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
             'judul' => 'required',
             'subjudul' => 'required',
-            'foto_update' => 'nullable|image|mimes:jpeg,png,jpg|max:2048'
+            'foto_update' => 'nullable|image|mimes:jpeg,png,jpg|max:5124'
         ];
     }
 
-    public function messages(): array
-    {
+    public function messages(): array {
         return [
             'judul_update.required' => 'Judul Event Wajib Diisi',
             'subjudul_update.required' => 'Keterangan Event Wajib Diisi',
             'foto_update.required' => 'foto Wajib Diisi',
             'foto_update.image' => 'Masukan harus berupa foto',
             'foto_update.mimes' => 'foto harus berupa file jpeg,png,jpg',
-            'foto_update.max' => 'Ukuran foto harus kurang 2MB',
+            'foto_update.max' => 'Ukuran foto harus kurang 5MB',
         ];
     }
 }
